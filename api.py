7fa61868e55c7ca2bbf96bbd4152f1fc6bee0bc7
botheredsid <bothered.sid@gmail.com>
2025-10-29 22:59:46 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 import os
-import chromedriver_autoinstaller
 from fastapi import FastAPI
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 app = FastAPI()
 
@@ -12,17 +12,16 @@ def home():
 
 @app.get("/browser")
 def browser_test():
-    # Install ChromeDriver automatically to a temporary path
-    chromedriver_autoinstaller.install(path="/tmp")
+    chrome_path = "/usr/bin/chromium"
+    driver_path = "/usr/bin/chromedriver"
 
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get("https://www.google.com")
     title = driver.title
     driver.quit()
